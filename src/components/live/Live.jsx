95df1c9a2ccf9267ccaf69c717ec489f9bf5fc59
MTuong_PB <49945491+Kyronk@users.noreply.github.com>
2023-05-30 08:33:15 +0700

    navbar fix

--- src/components/live/Live.jsx
+++ src/components/live/Live.jsx
@@ -1,11 +1,34 @@
 import React from 'react'
 
+import "./Live.scss";
+
+
 const Live = () => {
-  return (
-    <div>Live
-        hello this is live
-    </div>
-  )
+    return (
+        <>
+            <div className="live-body">
+                <div className='live-main'>
+                    <div className='live-content'>
+                        <h1>
+                            Mị thích là được ~ !
+                        </h1>
+                        <h2 className='h1'>
+                            Đây là quả thư viện của mị ~ <p>Nhé!</p>
+                        </h2>
+                        <h4>
+                            Thư viện lưu Anime&Manga
+                        </h4>
+                    </div>
+
+                    <div className='live-right'>
+                        <img src="https://i.imgur.com/ST7b3KM.png" alt="" />
+                        {/* <img src="https://img.wattpad.com/f15a59766baca5b010dc73addb981d9b025c3df5/68747470733a2f2f73332e616d617a6f6e6177732e636f6d2f776174747061642d6d656469612d736572766963652f53746f7279496d6167652f37657570354e4e4f4632564f41413d3d2d3833323431353536372e313565646365633537386537363062303438373335383638313731372e676966" alt="" /> */}
+                    </div>
+
+                </div>
+            </div>
+        </>
+    )
 }
 
 export default Live
--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -4,51 +4,62 @@ import { Link,NavLink } from "react-router-dom";
 
 
 const Nav = () => {
+
+    
+
     return (
         <>
-            <div className="nav_body">
+            
                 <header>
-                    <div className="nav_left">
-                        <h1>Logo</h1>
+                    <div className="nav_body">
+                        <div className='nav_logo'>
+                            <h1>Logo</h1>
+                        </div>
 
-                        <ul className='navbar'>
-                            <li>
-                                <NavLink>
-                                    <p>Home</p>
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink>
-                                    <p>Library</p>
-                                </NavLink>
-                            </li>
-                            <li>
-                                <NavLink>
-                                    <p>About</p>
-                                </NavLink>
-                            </li>
-                        </ul>
-                    </div>
-                    {/* <div className="navBar"> */}
+                        <div className="nav_left">
+                            <ul className='navbar'>
+                                <li>
+                                    <NavLink>
+                                        <p>Home</p>
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink>
+                                        <p>Library</p>
+                                    </NavLink>
+                                </li>
+                                <li>
+                                    <NavLink>
+                                        <p>About</p>
+                                    </NavLink>
+                                </li>
+                            </ul>
+                        </div>
+                        <div className='nav_arrow'>
 
+                        </div>
+
+
+                        <div className='nav_right'>
+                            <button
+                                className="btn_github"
+                            >
+                                <i className="fa fa-github"></i>
+                                Github
+                            </button>
+                            <button
+                                className="btn_source"
+                            >
+                                Source code
+                            </button>
+
+                            <i className="fa-solid fa-bars"></i>
                             
-                        
-                    {/* </div> */}
-                    <div className='nav_right'>
-                        <button
-                            className="btn_github"
-                        >
-                            <i className="fa fa-github"></i>
-                            Github
-                        </button>
-                        <button
-                            className="btn_source"
-                        >
-                            Source code
-                        </button>
+                        </div>
+
                     </div>
                 </header>
-            </div>
+        
         </>
     )
 }
